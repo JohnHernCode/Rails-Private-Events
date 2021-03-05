efda4df9e6fe00365e87c91f8b9f2a866dea663a
JohnHernCode <besthostandseoinfo@gmail.com>
2021-03-05 09:55:46 -0600

    Add models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'event/new'
+  get 'event/show'
+  get 'event/create'
+  get 'user/new'
+  get 'user/show'
+  get 'user/create'
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/user_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class UserControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get user_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get user_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get user_create_url
+    assert_response :success
+  end
+end

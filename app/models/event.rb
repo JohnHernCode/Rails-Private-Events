ee32ea2ee2b6db6c12e1edbd58f7c9b54d71548c
JohnHernCode <besthostandseoinfo@gmail.com>
2021-03-05 14:49:21 -0600

    Test Association Logic

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   
   has_many :attendances, foreign_key: 'attended_event_id'
-  has_many :attendees, through: :attendance, source: :event_attendee
+  has_many :attendees, through: :attendances, source: :event_attendee
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: 'creator_id', class_name: "Event"
  
   has_many :attendances, foreign_key: 'event_attendee_id'
-  has_many :attended_events, through: :attendance, source: :attended_event
+  has_many :attended_events, through: :attendances, source: :attended_event
 end

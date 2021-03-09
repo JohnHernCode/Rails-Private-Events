71678b7734190395a2c3add0508646a85634962c
JohnHernCode <besthostandseoinfo@gmail.com>
2021-03-09 15:38:22 -0600

    Work on Attend Test

--- test/controllers/event_controller_test.rb
+++ test/controllers/event_controller_test.rb
@@ -1,18 +1,33 @@
 require 'test_helper'
 
 class EventControllerTest < ActionDispatch::IntegrationTest
-  test 'should get new' do
-    get event_new_url
-    assert_response :success
+
+  setup do
+    @user = users(:two)
+    @event = events(:one)
   end
 
-  test 'should get show' do
-    get event_show_url
-    assert_response :success
-  end
+  # test 'should get new' do
+  #   get event_new_url
+  #   assert_response :success
+  # end
 
-  test 'should get create' do
-    get event_create_url
-    assert_response :success
+  # test 'should get show' do
+  #   get event_show_url
+  #   assert_response :success
+  # end
+
+  # test 'should get create' do
+  #   get event_create_url
+  #   assert_response :success
+  # end
+
+  test 'attend should create a new attendance' do
+    assert_difference('Attendance.count', 1) do
+       post attend_event_url(@user.id)
+      # @user.attended_events << @event
+    end
+    
+    # assert_redirected_to show_path
   end
 end

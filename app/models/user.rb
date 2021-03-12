68b01e1616b990725b7394b6ec9dff513b785c09
JohnHernCode <besthostandseoinfo@gmail.com>
2021-03-12 09:42:50 -0600

    Add more tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates_presence_of :username
+  # validates_presence_of :username
+  validates_uniqueness_of :username
+  validates :username, length: { minimum: 3 }
+
   has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
 
   has_many :attendances, foreign_key: 'event_attendee_id'

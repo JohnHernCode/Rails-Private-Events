745bc5016378c51c21ece2638ef64266fba92d61
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-03-12 19:17:05 -0300

    add event and user list helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def event_home(events)
+    html_code = ''
+    events.each do |event|
+      html_code << "<li><a href='events/#{event[:id]}'>#{event[:description]}</a>; #{event[:date]}</li>"
+    end
+    html_code.html_safe
+  end
 end
--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,8 +1,10 @@
 module EventHelper
-  def event_button
-    if logged_in? # needs && not creator && not past event
-      button_to 'Attend', action: :attend
-    end
+  def event_button(event, user)
+    return unless logged_in?
+    return if event[:creator_id] == user[:id]
+    return if event[:date] < Time.zone.now
+
+    button_to 'Attend', action: :attend
   end
 
   def show_events_loop
@@ -13,4 +15,5 @@ module EventHelper
     end
     html_values.html_safe
   end
+
 end

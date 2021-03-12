214b7ca899d886e63ecc9f8cbcf2c469a1136174
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-03-12 19:45:38 -0300

    add navbar and flash helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,32 @@ module ApplicationHelper
     end
     html_code.html_safe
   end
+
+  def flasher(flash)
+    if flash.now[:alert]
+      "<div class='notification is-danger global-notification'>
+        <p class='alert'>#{alert}</p>
+      </div>".html_safe
+    elsif flash.now[:notice]
+      "<div class='notification is-info global-notification'>
+        <p class='notice'> #{notice} </p>
+      </div>".html_safe
+    else
+      return
+    end
+  end
+
+  def nav_buttons
+    html_code = ''
+    if logged_in?
+      html_code << ('<span style="padding-right: 10px;">Hi! "<%= current_user.username.upcase %>"</span>')
+      html_code << (button_to "Logout", '/login', method: "delete")
+      html_code << (button_to "Create", '/event', method: :get)
+      html_code << (button_to "My Events", '/show', method: :get)
+    else
+      html_code << (button_to "Login", '/login', method: :get)
+      html_code << (button_to "Sign Up", new_user_path, method: :get)
+    end
+    html_code.html_safe
+  end
 end
